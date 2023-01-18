3af0d004ee079d6bbb4de1b5f2ab050391fd55a2
CrazyDeveloperTG <119148554+CrazyDeveloperTG@users.noreply.github.com>
2023-01-18 23:41:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MyBotGit/Elsa-Strange-mod.git /Elsa
+  git clone https://github.com/sathanxavier1998/req-repo.git /Elsa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Elsa
